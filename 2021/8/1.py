df2e3e362cd4415b0ff16c590f3ac56d1758015e
Robert Borkowski <rborkowski@linkedin.com>
2021-12-07 23:13:50 -0800

    Forgot strings are iterable

--- 2021/8/1.py
+++ 2021/8/1.py
@@ -14,44 +14,38 @@ class Display:
         for mapping in mappings:
             if len(mapping) == 2:
                 one = mapping
-                self.map[1] = self._to_set(mapping)
+                self.map[1] = set(mapping)
             elif len(mapping) == 3:
-                self.map[7] = self._to_set(mapping)
+                self.map[7] = set(mapping)
             elif len(mapping) == 4:
-                self.map[4] = self._to_set(mapping)
+                self.map[4] = set(mapping)
             elif len(mapping) == 7:
-                self.map[8] = self._to_set(mapping)
+                self.map[8] = set(mapping)
             elif len(mapping) == 5:
-                unknown_5.append(self._to_set(mapping))
+                unknown_5.append(set(mapping))
             elif len(mapping) == 6:
-                unknown_6.append(self._to_set(mapping))
+                unknown_6.append(set(mapping))
         for n in unknown_5:
             if self.map[1].issubset(n):
                 self.map[3] = n 
         unknown_5.remove(self.map[3])
         for n in unknown_6:
             if (one[0] in n and not one[1] in n) or (not one[0] in n and one[1] in n):
-                self.map[6] = self._to_set(n)            
+                self.map[6] = set(n)            
         unknown_6.remove(self.map[6])
         self.map[9] = self.map[3].union(self.map[4])
         unknown_6.remove(self.map[9])
-        self.map[0] = self._to_set(unknown_6[0])
+        self.map[0] = set(unknown_6[0])
         seg = self.map[8].difference(self.map[6]).pop()
         if seg in unknown_5[0]:
-            self.map[5] = self._to_set(unknown_5[1])
-            self.map[2] = self._to_set(unknown_5[0])
+            self.map[5] = set(unknown_5[1])
+            self.map[2] = set(unknown_5[0])
         else:
-            self.map[5] = self._to_set(unknown_5[0])
-            self.map[2] = self._to_set(unknown_5[1])
-            
-    def _to_set(self, word):
-        ret = set()
-        for c in word:
-            ret.add(c)
-        return ret
+            self.map[5] = set(unknown_5[0])
+            self.map[2] = set(unknown_5[1])
     
     def decode(self, word):
-        word_set = self._to_set(word)
+        word_set = set(word)
         id = self.map.index(word_set)
         return id
 
